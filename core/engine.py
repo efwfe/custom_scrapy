5fde659b12c03ef6a8f6833861989e1cc5ced9cc
spartazhangzhang <spartazhangzhang@gmail.com>
2018-05-18 20:42:37 +0800

    solve custom  spiders

--- core/engine.py
+++ core/engine.py
@@ -2,32 +2,35 @@
 from datetime import datetime
 
 from scrapy_custom.http.request import Request
-
-from .scheduler import Scheduler
+from scrapy_custom.middlewares import downloader_middlewares, spider_midllewares
+from scrapy_custom.utils.log import logger
 from .downloader import Downloader
 from .pipeline import Pipeline
-from scrapy_custom.middlewares import downloader_middlewares,spider_midllewares
-from scrapy_custom.utils.log import logger
+from .scheduler import Scheduler
+
 
 class Engine:
-    def __init__(self,spider):
-        self.spider = spider
+    def __init__(self, spiders):
+        self.spiders = spiders
         self.scheduler = Scheduler()
-        self.downloader =Downloader()
+        self.downloader = Downloader()
         self.pipeline = Pipeline()
         self.spider_mid = spider_midllewares.SpiderMidlleware()
-        self.downloader_mid  = downloader_middlewares.DownloaderMiddleware()
+        self.downloader_mid = downloader_middlewares.DownloaderMiddleware()
 
         self.total_response_number = 0
 
     def _start_requests(self):
-        for start_request in self.spider.start_requests():
-            start_request = self.spider_mid.process_request(start_request)
-            # add to scheduler
-            self.scheduler.add_request(start_request)
+
+        for spider_name, spider in self.spiders.items():
+            for start_request in spider.start_requests():
+                start_request = self.spider_mid.process_request(start_request)
+                # add name to the request && add to scheduler
+                start_request.spider_name = spider_name
+                self.scheduler.add_request(start_request)
 
     def _execute_request_response_item(self):
-        request =self.scheduler.get_request()
+        request = self.scheduler.get_request()
         if request is None:
             return
 
@@ -40,20 +43,20 @@ class Engine:
         # downloader process response
         response = self.downloader_mid.process_response(response)
 
-
+        # get the spider
+        spider = self.spiders[request.spider_name]
         # user custom parse
-        parse = getattr(self.spider, request.parse)
+        parse = getattr(spider, request.parse)
         results = parse(response)
 
-
         for result in results:
-            if isinstance(result,Request):
+            if isinstance(result, Request):
                 #  is Request back to scheduler
                 result = self.spider_mid.process_request(result)
                 self.scheduler.add_request(result)
             else:
                 # pipeline solve the result
-                result =self.spider_mid.process_response(result)
+                result = self.spider_mid.process_response(result)
                 self.pipeline.process_item(result)
 
         self.total_response_number += 1
@@ -64,7 +67,6 @@ class Engine:
             self._execute_request_response_item()
             if self.total_response_number >= self.scheduler.total_request_number:
                 break
-
 
     def start(self):
         start = datetime.now()  # 起始时间
